4b236d7b209952c00bbdcd79dd5e331fc2c09e70
BoredCreator <63941499+BoredCreator@users.noreply.github.com>
2023-01-08 07:03:01 -0800

    Final polish, making sure various functions perform as intended Co-authored-by: Tanuj Siripurapu <tanujsiripurapu@gmail.com>

--- src/screens/parent/index.js
+++ src/screens/parent/index.js
@@ -15,14 +15,15 @@ import * as Progress from "react-native-progress";
 import Svg, { Ellipse, ClipPath } from "react-native-svg";
 import * as Clipboard from "expo-clipboard";
 import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  interpolate,
-  withTiming,
-  withDelay,
-  runOnJS,
-  withSequence,
-  withSpring,
+  SlideInDown,
+  SlidenInUp,
+  SlideInLeft,
+  SlideInRight,
+  LightSpeedOutLeft,
+  LightSpeedOutRight,
+  SlideOutUp,
+  SlideOutDown,
+  Transition,
 } from "react-native-reanimated";
 import { Feather } from "@expo/vector-icons";
 import { useDispatch } from "react-redux";
@@ -67,102 +68,126 @@ export default function ParentScreen() {
   );
   const renderItem = ({ item }) => <Item item={item} />;
   return (
-    <View style={{ flex: 1, backgroundColor: "black" }}>
-      <Image style={StyleSheet.absoluteFillObject} source={{ uri: bg_image }} />
-      <View style={{ flex: 0.8 }}>
-        <FlatList
-          keyExtractor={(item) => item.id}
-          data={childrenArray}
-          renderItem={({ item, index }) => {
-            const inputRange = [
-              -1,
-              0,
-              ITEM_SIZE * index,
-              ITEM_SIZE * (index + 2),
-            ];
-            return (
-              <View style={styles.flatList}>
-                <View style={{ flex: 1, flexDirection: "column" }}>
-                  <TouchableOpacity style={styles.flatListButtonContainer}>
-                    <Text
-                      style={{
-                        fontSize: 25,
-                        color: "white",
-                      }}
-                    >
-                      {console.log(item)}
-                      {item.name}
-                    </Text>
-                    <Text style={styles.subHeader}></Text>
-                    <Text style={styles.dubHeader}>{item.suggestion}</Text>
-                    <View
-                      style={{ flexDirection: "row", justifyContent: "center" }}
-                    >
-                      <Text> </Text>
-                      <Text style={styles.dubHeader}>Mood Rating : </Text>
-                      <Text style={styles.dubHeader}>{item[2]}</Text>
-                      <Progress.Bar
-                        progress={item[0]}
-                        width={200}
-                        height={20}
-                        unfilledColor={"#90EE90"}
-                        borderColor={"#000000"}
-                        color={"#a83232"}
-                      />
-                    </View>
-                  </TouchableOpacity>
-                </View>
-              </View>
-            );
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "black",
+        alignContent: "center",
+        justifyContent: "center",
+      }}
+    >
+      <View
+        style={{
+          flex: 0.8,
+          backgroundColor: "rgb(17, 51, 46)",
+          borderRadius: 40,
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            borderColor: "rgb(237, 229, 204)",
+            borderRadius: 40,
+            borderWidth: 1,
           }}
-        />
-        <Modal isVisible={isModalVisible}>
-          <View
-            style={{
-              justifyContent: "center",
-              alignContent: "center",
-              flex: 1,
+        >
+          <FlatList
+            keyExtractor={(item) => item.id}
+            data={childrenArray}
+            renderItem={({ item, index }) => {
+              const inputRange = [
+                -1,
+                0,
+                ITEM_SIZE * index,
+                ITEM_SIZE * (index + 2),
+              ];
+              return (
+                <Animated.View entering={SlideInLeft} style={styles.flatList}>
+                  <View style={{ flex: 1, flexDirection: "column" }}>
+                    <TouchableOpacity style={styles.flatListButtonContainer}>
+                      <Text
+                        style={{
+                          fontSize: 25,
+                          color: "rgb(17, 51, 46)",
+                        }}
+                      >
+                        {console.log(item)}
+                        {item.name}
+                      </Text>
+                      <Text style={styles.subHeader}></Text>
+                      <Text style={styles.dubHeader}>{item.suggestion}</Text>
+                      <View
+                        style={{
+                          flexDirection: "row",
+                          justifyContent: "center",
+                        }}
+                      >
+                        <Text> </Text>
+                        <Text style={styles.dubHeader}>Health Rating : </Text>
+                        <Text style={styles.dubHeader}>{item[2]}</Text>
+                        <Progress.Bar
+                          progress={item[0]}
+                          width={200}
+                          height={20}
+                          unfilledColor={"#90EE90"}
+                          borderColor={"#000000"}
+                          color={"#a83232"}
+                        />
+                      </View>
+                    </TouchableOpacity>
+                  </View>
+                </Animated.View>
+              );
             }}
-          >
-            <Text style={styles.header}>Enter Code Here</Text>
-            <View style={{ opacity: 1 }}>
-              <TextInput
-                placeholderTextColor={"Grey"}
-                placeholder={"User Id"}
-                style={styles.inputContainer}
-                onChangeText={(value) => setUserid(value)}
-              />
+          />
+          <Modal isVisible={isModalVisible}>
+            <View
+              style={{
+                justifyContent: "center",
+                alignContent: "center",
+                flex: 1,
+              }}
+            >
+              <Text style={styles.header}>Enter Code Here</Text>
+              <View style={{ opacity: 1 }}>
+                <TextInput
+                  placeholderTextColor={"rgb(17, 51, 46)"}
+                  placeholder={"User ID"}
+                  style={styles.inputContainer}
+                  onChangeText={(value) => setUserid(value)}
+                />
+              </View>
+              <Text style={styles.header}>Enter Child's name Here</Text>
+              <View style={{ opacity: 1 }}>
+                <TextInput
+                  placeholderTextColor={"rgb(17, 51, 46)"}
+                  placeholder={"Username"}
+                  style={styles.inputContainer}
+                  onChangeText={(value) => setUserName(value)}
+                />
+              </View>
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => {
+                    handleInviteUser();
+                    handleModal;
+                  }}
+                >
+                  <Text style={styles.circleText}>Add</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={handleModal}>
+                  <Text style={styles.circleText}>Close</Text>
+                </TouchableOpacity>
+              </View>
             </View>
-            <Text style={styles.header}>Enter Child's name Here</Text>
-            <View style={{ opacity: 1 }}>
-              <TextInput
-                placeholderTextColor={"Grey"}
-                placeholder={"User Name"}
-                style={styles.inputContainer}
-                onChangeText={(value) => setUserName(value)}
-              />
-            </View>
-            <View style={styles.buttonContainer}>
-              <TouchableOpacity
-                style={styles.button}
-                onPress={() => {
-                  handleInviteUser();
-                  handleModal;
-                }}
-              >
-                <Text style={styles.circleText}>Add</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.button} onPress={handleModal}>
-                <Text style={styles.circleText}>Close</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </Modal>
+          </Modal>
+        </View>
       </View>
       <TouchableOpacity
         onPress={setIsModalVisible}
         style={{
-          backgroundColor: "white",
+          backgroundColor: "rgb(237, 229, 204)",
           borderRadius: 10,
           borderWidth: 4,
           padding: 15,
